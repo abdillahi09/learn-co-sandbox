45da47a523198dd29d7ebaa7878ecc03b57d4f33
abdillahi Bashir <abdillahi.muse@hotmail.com>
2018-08-01 04:59:46 +0000

    automatically backed up by learn

---
+++ callback.js
@@ -0,0 +1,13 @@
+var nails = ["rusty nail", "rusty fingers", "bent nail", "clean nail"];
+
+
+for (var i = 0; i < nails.length; i++) {
+  clean(nails[i])
+}
+
+
+ function clean(item) {
+  console.log(`I just cleaned a ${item}`)
+}
+
+
